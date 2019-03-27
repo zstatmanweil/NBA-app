afcae86c2ad201e908253c7b0aec4656d4054a08
zstatmanweil <zstatmanweil@gmail.com>
2019-03-27 12:48:12 -0400

    got application to work

--- app.py
+++ app.py
@@ -1,20 +1,43 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, escape, request
 from flask import render_template
-from flask import request
+import json
+from nba.app_functions import display_table, convert_to_players, users_choice
+
+#npaplayers.json found here: http://data.nba.net/10s/prod/v1/2017/players.json
+json_file = open('./data/nbaplayers.json')
+json_str = json_file.read()
+json_data = json.loads(json_str)
+
+#accessing each player's dictionary and name
+players = json_data['league']['standard']
+
+# Get list of player objects
+players_list = convert_to_players(players)
 
 app = Flask(__name__)
 
-@app.route("/hello", methods=['POST', 'GET'])
+@app.route("/")
 def index():
-    greeting = 'Hello World'
+    return render_template('index.html')
 
-    if request.method == 'POST':
-        name = request.form['name']
-        greet = request.form['greet']
-        greeting = f'{greet}, {name}!'
-        return render_template('index.html', greeting=greeting)
-    else:
-        return render_template("hello_form.html", greeting=greeting)
+@app.route("/start", methods=['GET'])
+def start():
+    first_table = display_table(players_list)
+    first_question = """\nAre you curious which players were drafted in a specific year? Of \
+course you are! Enter a year, and a list of players drafted that year will be provided."""
+
+    return render_template('choice_form.html', first_table=first_table.to_html(), first_question=first_question)
+
+@app.route("/choice", methods=['GET'])
+def choice():
+    return render_template('choice_form.html')
+
+@app.route("/result", methods=['POST'])
+def result():
+    choice = request.form['choice']
+    result = users_choice(players_list, choice)
+
+    return render_template('show_result.html', result=result)
 
 if __name__ == "__main__":
     app.run()
---
+++ nba/app_functions.py
@@ -0,0 +1,106 @@
+from nba.player import Player
+from nba.choice import Choice
+import pandas as pd
+
+#converts players created from the Player class into a list of players
+def convert_to_players(raw_data):
+    players = []
+    for player in raw_data:
+        player = Player(
+                player['firstName'],
+                player['lastName'],
+                player['draft']['seasonYear'],
+                player['draft']['pickNum'],
+                player['draft']['roundNum']
+                )
+        players.append(player)
+    return players
+
+#creates a table showing each year a player was drafted and how many players
+#were drafted that year
+def display_table(players_list):
+    draft_years = []
+    draft_years_count = []
+    
+    for player in players_list:
+        draft_year = player.draft_year
+        
+        if draft_year != '':
+            draft_year = int(draft_year)
+        else:
+            draft_year = 0
+        
+        if draft_year not in draft_years:
+            draft_years_count.append(1)
+            draft_years.append(draft_year)
+        else:
+            i = list(draft_years).index(draft_year)
+            draft_years_count[i] += 1     
+    
+    #creating and organizing data table
+    data = {'No. of Players' : draft_years_count}
+    sum_table = pd.DataFrame(data, draft_years)
+    sum_table_sorted = sum_table.sort_index(ascending=True)
+    total_players = sum_table_sorted["No. of Players"].sum()
+    players_no_data = sum_table_sorted.loc[0, "No. of Players"]
+    sum_table_clean = sum_table_sorted.drop(0, axis=0)
+            
+#     result = f"""\nHere are the number of players still in the league \
+# from each draft year as of 2017. Note: there wasn't draft data for \
+# {players_no_data} of the {total_players} players. It is possible these players \
+# weren't drafted. \n"""
+
+    return sum_table_clean
+
+#creates a dictionary for each year a player was drafted and all players 
+#drafted that year        
+def players_by_year(players_list):
+    draft_years_players = {}
+
+    for player in players_list:
+        draft_year = player.draft_year
+    
+        if draft_year != '':
+            draft_year = int(draft_year)
+        else:
+            draft_year = 0
+        
+        if draft_year in draft_years_players:
+            draft_years_players[draft_year].append(player)
+        else:
+            draft_years_players[draft_year] = [player]
+
+    return draft_years_players
+
+# finds the user's player within the player list, if it exists    
+def find_player_by_name(players_list, user_submitted_name):
+        for player in players_list:
+            if user_submitted_name.lower() == player.search_name():
+                return player
+        return False
+
+
+def users_choice(players_list, user_choice):
+    choice = Choice(user_choice)
+    result = ''
+    if choice.is_NBA_year():
+        players = players_by_year(players_list).get(int(choice.user_choice))
+        if players:
+            result = "\n" + ", ".join(map(lambda p: p.full_name(), players))
+        else:
+            result = f"\nNo one drafted in {choice} was in the NBA as of 2017"
+
+    elif choice.is_year_prior_NBA():
+        result = "\nThat is prior to the NBA's existance"
+
+    elif choice.is_name():
+        player = find_player_by_name(players_list, choice.user_choice)
+        if player:
+            result = player.draft_info()
+        else:
+            result = f"\n{choice.user_choice.title()} wasn't active in the NBA in 2017."
+
+    else:
+        result = "Try again!"
+
+    return result
